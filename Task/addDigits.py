607af0e3727b2760dd945ea3ea2cc89c347db906
v.borys <v.borys@paytechlab.com>
2025-05-22 13:58:59 +0300

    update

--- Task/addDigits.py
+++ Task/addDigits.py
@@ -11,9 +11,15 @@ class Solution(object):
             num = sum(int(digit) for digit in str(num))
         return num
 
+@allure.epic("User Management")
 @allure.feature("FEATURE")
 @allure.story("STORY")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.label("owner", "v.borys")
+@allure.label("layer", "e2e")
+@allure.link("https://gitlab.com/valentinborys/education", name="GitLab Project")
+@allure.issue("EDU-123", "Фиксим баг при логине")
+@allure.tag("regression", "smoke")
 def test_unit():
     solution = Solution()
     print(solution.addDigits(4))
